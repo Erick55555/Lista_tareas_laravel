602885fc2cee3b3db1534390f0b110900cb9e29b
Aaron <agomezdo20dw@ikzubirimanteo.com>
2021-11-10 10:30:50 +0100

    Subiendo Lista_tareas_laravel

---
+++ lista_tareas_proyecto/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\tareasControlador;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [tareasControlador::class , "mostrar"])->name("ruta_tarea_mostrar");;
+
+
+
+Route::post('/tarea', [tareasControlador::class , "anadir"])->name("ruta_tarea_anadir");
+
+Route::delete('/task/{id}', [tareasControlador::class , "eliminar"]) ->name("ruta_tarea_eliminar");;
